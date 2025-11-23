3bc16376024254f409c56c7c96f72004629ab354
GeorgiyKuzora <g.kuzora@korona.net>
2025-11-23 11:06:46 +0300

    add dynamic arguments

--- pkg/args/args.go
+++ pkg/args/args.go
@@ -22,8 +22,22 @@ func (a *Args) ToConfig() config.Config {
 
 func GetArgs() (Args, error) {
 	args := os.Args
-	if len(args) < 4 {
-		return Args{}, errors.New("there should be at least 3 arguments")
+	var filepath, topic, url string
+	args_len := len(args)
+	if args_len < 2 {
+		return Args{}, errors.New("there should be at least 1 argument")
+	} else if args_len < 3 {
+		filepath = args[1]
+		topic = ""
+		url = ""
+	} else if args_len < 4 {
+		filepath = args[2]
+		topic = args[1]
+		url = ""
+	} else {
+		filepath = args[3]
+		topic = args[2]
+		url = args[1]
 	}
-	return Args{Url: args[1], FilePath: args[2], Topic: args[3]}, nil
+	return Args{Url: url, FilePath: filepath, Topic: topic}, nil
 }
